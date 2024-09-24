b6723794cbdc975ad995abd836ba6b9c0e1ee3a9
wqtwjt1996 <1061687050@qq.com>
2024-09-23 20:28:50 -0400

    Update split_by_country.py

--- split_by_country.py
+++ split_by_country.py
@@ -2,7 +2,7 @@ import geopandas as gpd
 
 original_shp_file_path = '/Users/wangqitong/学习资料/Fall2024_Geo_Transfer/Jul2024_QGIS/Co/bbox_v1_Co.shp'
 output_shp_file_path = '/Users/wangqitong/学习资料/Fall2024_Geo_Transfer/Sep2024_QGIS_Country_Split/Co/bbox_v1_Co_DRC.shp'  #
-id_list = ['2010', '2020', '2030', '2040', '2050']# 输出文件路径
+id_list = ['2010', '2020', '2030', '2040', '2050']
 
 def filter_geodataframe_by_id_pattern(original_gdf, output_path, id_list):
     mask = original_gdf['id'].astype(str).str.startswith(tuple(id_list))
